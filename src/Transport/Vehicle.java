784794f7c592852dd928843833d01ebb74d9862d
RJ6776-AKJ <rj6776jha@gmail.com>
2026-09-07 22:14:12 +0530

    adding new lecture to OOPs

---
+++ src/Transport/Vehicle.java
@@ -0,0 +1,13 @@
+package Transport;
+
+public class Vehicle {
+    public String name;
+
+    public String model;
+
+    public int noOfTyres;
+
+    Vehicle (String name, String model, int noOfTyres){
+
+    }
+}
