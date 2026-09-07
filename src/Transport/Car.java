ec34a484286cf39a50c9901f91642aa81171269a
RJ6776-AKJ <rj6776jha@gmail.com>
2026-09-07 22:26:17 +0530

    adding something new

---
+++ src/Transport/Car.java
@@ -0,0 +1,15 @@
+package Transport;
+
+public class Car extends Vehicle {
+
+    public int  noOfDoors;
+
+    public String tranmissionType;
+
+
+
+
+
+
+
+}
--- src/Transport/Vehicle.java
+++ src/Transport/Vehicle.java
@@ -7,7 +7,29 @@ public class Vehicle {
 
     public int noOfTyres;
 
-    Vehicle (String name, String model, int noOfTyres){
-
+    Vehicle (){
+        this.name = "";
+        this.model= "";
+        this.noOfTyres= -1;
     }
+    Vehicle (String name, String model, int noOfTyres){
+        this.name = name;
+        this.model=model;
+        this.noOfTyres=noOfTyres;
+    }
+
+    void startEngine(){
+        System.out.printf("Engine is starting of %s : %s\n", name,model);
+    }
+
+
+
+    void stopEngine(){
+        System.out.printf("Engine is starting of %s : %s\n", name,model);
+    }
+
+
+
+
+
 }
