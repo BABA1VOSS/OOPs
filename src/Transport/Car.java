ba4c9e306fd7242b7b981c979830547e7a4544f2
RJ6776-AKJ <rj6776jha@gmail.com>
2026-09-09 20:13:48 +0530

    inculsive new comments for this

--- src/Transport/Car.java
+++ src/Transport/Car.java
@@ -11,6 +11,8 @@ public class Car extends Vehicle {
         //mein vehicle ka ctor() call karna padega, taaki car ke corresponding vehcile ke public attribute call kar saaku..
         //iske lie hum SUPER keyword use karte hai--> super kyewords in cheezon ke lie shandaar hai
         // ++ ctor body mein super() keyword pehli matlab first body mein aani chaiye --> call to 'super() must be first Statement in constructor body.
+        // hierarchy ke hisab se soche agar oops ko life mein implement kar ke dekhe to hum isko dekh sakte hai ki pehle parents ki properties pehle inherit hoti hai 
+
         super(name, model, noOfTyres);
         this.noOfDoors = noOfDoors;
         this.tranmissionType = transmissionType;
