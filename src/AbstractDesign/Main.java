326fddf7e807e6813ed86cf806b370d717c5cbff
RJ6776-AKJ <rj6776jha@gmail.com>
2026-09-18 16:59:55 +0530

    Learning ABStraction

---
+++ src/AbstractDesign/Main.java
@@ -0,0 +1,100 @@
+//package AbstractDesign;
+//
+//
+//abstract class Bird{
+//    //pehle template banegi
+//    abstract void fly();
+//        abstract void eat();
+//}
+//class Sparrow extends Bird {
+//
+//    @Override
+//    //fir definition denge common interface ka
+//    void fly() {
+//        System.out.println("Sparrow flying in a different way");
+//    }
+//
+//    @Override
+//    void eat() {
+//        System.out.println("sparrow eating ");
+//    }
+//}
+//
+//class Crow extends Bird {
+//
+//    @Override
+//        //fir definition denge common interface ka
+//    void fly() {
+//        System.out.println("Crow flying");
+//    }
+//
+//    @Override
+//    void eat() {
+//        System.out.println("Crow eating in alag way ");
+//    }
+//}
+//public class Main{
+//    public static void doBirdStuff(Bird b) {
+//        b.eat();
+//        b.fly();
+//    }
+//    public static void main(String[] args) {
+//     b.eat();
+//      b.fly();
+//
+//        //yeh polymorphism wala tareeke se call kia hai humne ab
+//        doBirdStuff(new Sparrow());
+//        doBirdStuff(new Crow());
+//    }
+//}
+
+
+// Interface
+package AbstractDesign;
+
+interface Bird {
+
+    void fly();
+    void eat();
+        }
+
+
+
+ //ABSTRACT CLASS AB PURANA JAVA KA CODE HO GAYA HAI ..
+ class Sparrow implements Bird {
+
+
+     @Override
+     public void fly() {
+         System.out.println("Sparrow flying");
+     }
+
+     @Override
+     public void eat() {
+         System.out.println("Crow eating");
+     }
+ }
+
+class Crow implements Bird {
+    //fir definition denge common interface ka
+    @Override
+    public void fly() {
+        System.out.println("Crow flying  234");
+    }
+
+    @Override
+    public void eat() {
+        System.out.println("Crow eating in alag way ");
+    }
+}
+public class Main{
+    public static void doBirdStuff(Bird b) {
+        b.eat();
+        b.fly();
+    }
+    public static void main(String[] args) {
+
+        doBirdStuff(new Sparrow());
+        doBirdStuff(new Crow());
+    }
+}
