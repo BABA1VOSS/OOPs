1e4cb55298e8caff414bcc6ff502d0bc227a4f65
RJ6776-AKJ <rj6776jha@gmail.com>
2026-09-18 17:34:12 +0530

    hello

---
+++ src/AbstractDesign/AbstractClass.java
@@ -0,0 +1,56 @@
+package AbstractDesign;
+
+
+abstract class Birds{
+    //pehle template banegi
+    abstract void fly();
+    abstract void eat();
+    public void sleep(){
+        System.out.println("bird sleeping");
+    }
+}
+class Sparrows extends Bird {
+
+    @Override
+        //fir definition denge common interface ka
+    void fly() {
+        System.out.println("Sparrow flying in a different way");
+    }
+
+    @Override
+    void eat() {
+        System.out.println("sparrow eating ");
+    }
+}
+
+class Crows extends Bird {
+
+    @Override
+        //fir definition denge common interface ka
+    void fly() {
+        System.out.println("Crow flying");
+    }
+
+    @Override
+    void eat() {
+        System.out.println("Crow eating in alag way ");
+    }
+}
+
+
+
+public class AbstractClass {
+    public static void doBirdStuff(Bird b) {
+        b.eat();
+        b.fly();
+        b.sleep();
+    }
+    public static void main(String[] args) {
+//      b.eat();
+//      b.fly();
+
+        //yeh polymorphism wala tareeke se call kia hai humne ab
+        doBirdStuff(new Sparrow());
+        doBirdStuff(new Crow());
+    }
+}
